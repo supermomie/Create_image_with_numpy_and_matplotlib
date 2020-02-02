a91576581c55e79fb6b1e80df22b4c7615c96314
supermomie <fakhredinedev3.0@gmail.com>
2020-02-02 17:21:06 +0100

    start creating rectangle mode

--- plot.py
+++ plot.py
@@ -3,43 +3,46 @@ import matplotlib.pyplot as plt
 
 
 FOLDER = "./img/"
-NAME = FOLDER+"drapeau"
-H = 2000
-W = 2000
+NAME = "drapeau"
+PATH = FOLDER+NAME
+H = 10
+W = 23
 
-BLUE   = [0, 0, 255]
-GREEN  = [0, 255, 0]
 RED    = [255, 0, 0]
+GREEN  = [0, 255, 0]
+BLUE   = [0, 0, 255]
 YELLOW = [255, 255, 0]
-WHITE = [255, 255, 255]
+WHITE  = [255, 255, 255]
+CYAN   = [0, 255, 255]
 
 
 
 def change_values(newImage, h):
     for i in range(int(h)//2):
-        newImage[0+i,1+i: -i-1: ] = 3 #CHANGE TOP SIDE VALUES
-        newImage[i,0 : i : 1] = 4     #CHANGE TOPLEFT SIDE VALUES
-        newImage[-i-1,0 : i : 1] = 4  #CHANGE BOTTOMLEFT SIDE VALUES
-        newImage[-i,i: -i: ] = 5      #CHANGE BOTTOM SIDE VALUES
+        newImage[0+i,1+i: -i-1: ] = 3     #CHANGE TOP SIDE VALUES
+        newImage[i,0 : i : 1]     = 4     #CHANGE TOPLEFT SIDE VALUES
+        newImage[-i-1,0 : i : 1]  = 4     #CHANGE BOTTOMLEFT SIDE VALUES
+        newImage[-i,i: -i: ]      = 5     #CHANGE BOTTOM SIDE VALUES
+        newImage[-i,i: i:  ]      = 2     #CHANGE RIGHT SIDE VALUES
 
 
-def create_img(name, h, w):
+def create_img(path, name, h, w):
     image = np.zeros((h, w, 3))
-    diag = np.eye(w, h, dtype=int)
+    diag = np.eye(h, w, dtype=int)
     newImage = diag + np.flipud(diag)
 
     change_values(newImage, h)
     image[np.where(newImage == 0)] = RED
     image[np.where(newImage == 1)] = WHITE
+    image[np.where(newImage == 2)] = CYAN
     image[np.where(newImage == 3)] = BLUE
     image[np.where(newImage == 4)] = YELLOW
     image[np.where(newImage == 5)] = GREEN
-    #print(newImage)
+    print(newImage)
     plt.title(name)
     plt.imshow(image)
-    plt.savefig(name)
+    plt.savefig(path)
     plt.show()
 
-
 if __name__ == "__main__":
-    create_img(NAME, H, W)
+    create_img(PATH, NAME, H, W)
